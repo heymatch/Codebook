f4ab7703dbf013555194410a69c1e08de5cc722d
heymatch <match.remo@gmail.com>
2019-10-12 20:56:10 +0800

    add

---
+++ codebook/Code/string.cpp
@@ -0,0 +1,22 @@
+//init
+string
+//iterator
+iterator begin()
+iterator end()
+//capacity
+size_type size()
+void reserve(size_type)
+bool empty()
+//access
+reference operator[](size_type)
+reference at(size_type)
+//modifiers
+string operator+= (string)
+string insert(pos, string)
+string erase(pos = 0, len)
+//opeartion
+string substr(pos = 0, len)
+//function
+string to_string(val)
+//stringstream
+string str()
