a77cbfe99caf0d2795b7279cc815fc2c004c8b1e
heymatch <match.remo@gmail.com>
2020-11-23 23:43:47 +0800

    temp remove

--- codebook/Code/compile.sh
+++
@@ -1,5 +0,0 @@
-#	shell script to compile program and execute
-#	execute: bash compile.sh $filename 
-
-#!/bin/bash
-g++ -Wall -O2 -std=c++14 -static -pipe -g -o $1 $1.cpp && ./$1 < $1.in > $1.out && more ./$1.out
--- codebook/Code/copy.sh
+++
@@ -1,8 +0,0 @@
-#	copy template file
-#!bin/bash
-for name in {A..M};
-do
-	cp template.cpp $name.cpp
-	echo 0 > $name.in
-	echo 0 > $name.out
-done
